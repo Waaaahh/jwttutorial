bb2d412f8be37acea85d0c7406d774db0ecafbf6
cws <chws900@gmail.com>
2022-10-03 23:21:47 +0900

    JWT 필터 TokenProvider 작성

--- jwttutorial/src/main/java/com/jwttoken/tutorial/jwttutorial/config/SecurityConfig.java
+++ jwttutorial/src/main/java/com/jwttoken/tutorial/jwttutorial/config/SecurityConfig.java
@@ -8,12 +8,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    // @Override
-    // public void configure(WebSecurity web) throws Exception {
-    // web
-    // .ignoring()
-    // .antMatchers("/h2-console/**", "/favicon.ico");
-    // }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**", "/favicon.ico");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
---
+++ jwttutorial/src/main/java/com/jwttoken/tutorial/jwttutorial/jwt/JwtFilter.java
@@ -0,0 +1,32 @@
+package com.jwttoken.tutorial.jwttutorial.jwt;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.filter.GenericFilterBean;
+
+public class JwtFilter extends GenericFilterBean {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    private TokenProvider tokenProvider;
+
+    public JwtFilter(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+    }
+
+}
